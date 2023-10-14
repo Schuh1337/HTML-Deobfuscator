d9ec937586c532b3d1027155fa6cab9d1f6a6479
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-14 23:15:07 +0200

    Add files via upload

---
+++ deobf.py
@@ -0,0 +1,19 @@
+import re
+import html
+import os
+
+def deobfuscate(obfuscated):
+    decode = html.unescape(obfuscated)
+    decode = re.sub(r'<script>.*?</script>', '', decode, flags=re.DOTALL)
+    decode = re.sub(r'<noscript>.*?</noscript>', '', decode, flags=re.DOTALL)
+    return decode
+
+obfuscated = """ HERE """  # <-- full obfuscated code here
+
+deobf = deobfuscate(obfuscated)
+
+path = 'deobf.txt'
+with open(path, 'w', encoding="utf-8") as file:
+    file.write(deobf)
+print(f"Deobfuscated code has been saved to {os.path.abspath(path)}")
+input()
